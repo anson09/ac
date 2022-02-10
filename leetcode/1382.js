dd896bef8522e37362d2defdf0af66df85d8f07d
anson <anson@ricequant.com>
2022-02-10 11:28:34 +0800

    feat: init leetcode link

---
+++ leetcode/1382.js
@@ -0,0 +1,66 @@
+// https://leetcode-cn.com/problems/balance-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+
+function isValid(node) {
+  function depth(root) {
+    if (!root) return 0;
+    let left = depth(root.left);
+    if (left === -1) {
+      return -1;
+    }
+    let right = depth(root.right);
+    if (right === -1) {
+      return -1;
+    }
+    if (Math.abs(left - right) > 1) return -1;
+    return Math.max(left, right) + 1;
+  }
+  if (depth(node) === -1) return false;
+  return true;
+}
+
+function preOrder(root, list = []) {
+  if (!root) return list;
+  if (root.left) preOrder(root.left, list);
+  list.push(root);
+  if (root.right) preOrder(root.right, list);
+  return list;
+}
+
+function build(list, root, side) {
+  if (!list.length) return root;
+  let mid = Math.floor(list.length / 2);
+  if (!root) {
+    root = list[mid];
+  } else {
+    root[side] = list[mid];
+    root = root[side];
+  }
+  build(list.slice(0, mid), root, "left");
+  build(list.slice(mid + 1, list.length), root, "right");
+  return root;
+}
+
+var balanceBST = function (root) {
+  if (isValid(root)) return root;
+
+  const sortedList = preOrder(root).map((i) => {
+    i.left = null;
+    i.right = null;
+    return i;
+  });
+
+  return build(sortedList);
+};
