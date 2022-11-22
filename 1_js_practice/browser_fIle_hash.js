a1a76e86139a108148091161ccaaed9ab9e8fd97
anson <anson@ricequant.com>
2022-11-22 18:31:22 +0800

    refactor: merge dir

--- 1_js_practice/browser_fIle_hash.js
+++ 1_js_practice/browser_fIle_hash.js
@@ -1,9 +1,9 @@
-// 使用浏览器 API 生成文件 SHA1 哈希值
+// 使用浏览器 API 生成文件 SHA-256 哈希值
 async function getFileHash() {
   const [fileHandle] = await showOpenFilePicker();
   const file = await fileHandle.getFile();
   const fileBinary = await file.arrayBuffer();
-  const hashBinary = await crypto.subtle.digest("SHA-1", fileBinary);
+  const hashBinary = await crypto.subtle.digest("SHA-256", fileBinary);
   return [...new Uint8Array(hashBinary)]
     .map((i) => i.toString(16).padStart(2, "0"))
     .join("");
