a7d9f3c960517365485569416581f25f42536bc3
anson <anson@ricequant.com>
2022-12-05 00:10:55 +0800

    refactor: big_positive_number_add

--- 3_algorithm/big_positive_number_add.js
+++ 3_algorithm/big_positive_number_add.js
@@ -1,21 +1,11 @@
 // 大正数相加
-const assert = require("node:assert/strict");
-
 function integerAdd(a, b) {
-  const sa = a.split("").map(Number).reverse();
-  const sb = b.split("").map(Number).reverse();
-  let larger, smaller;
-  if (sa.length > sb.length) {
-    larger = sa;
-    smaller = sb;
-  } else {
-    larger = sb;
-    smaller = sa;
-  }
+  const [sa, sb] = [a, b].map((i) => i.split("").map(Number).reverse());
+  const [larger, smaller] = sa.length >= sb.length ? [sa, sb] : [sb, sa];
+
   return larger
     .reduce((prev, cur, idx) => {
-      // 进位
-      const carry = prev[idx] ? 1 : 0;
+      const carry = prev[idx] ? 1 : 0; // 进位
       const bitSum =
         idx < smaller.length ? cur + smaller[idx] + carry : cur + carry;
       prev[idx] = bitSum % 10;
@@ -26,11 +16,10 @@ function integerAdd(a, b) {
     .join("");
 }
 
-assert.equal(integerAdd("1", "9999"), integerAdd("10", "9990"));
-
 function add(a, b) {
   let [ai, ad] = a.split(".");
   let [bi, bd] = b.split(".");
+
   if (!ad && !bd) return integerAdd(ai, bi);
   if (!ad) return integerAdd(ai, bi) + "." + bd;
   if (!bd) return integerAdd(ai, bi) + "." + ad;
@@ -40,16 +29,28 @@ function add(a, b) {
     ad.padEnd(decimalLength, "0"),
     bd.padEnd(decimalLength, "0")
   );
+
   let carry = "0";
   if (decimalSum.length > decimalLength) {
     carry = "1";
     decimalSum = decimalSum.slice(1);
   }
-  decimalSum = /(\d*)(?<=(?:^|[1-9]))0*/.exec(decimalSum)[1];
+
+  // 去除小数点尾部 0
+  for (var i = decimalSum.length - 1; i >= 0; i--) {
+    if (decimalSum[i] !== "0") break;
+  }
+  decimalSum = i === -1 ? "" : decimalSum.slice(0, i + 1);
+
   return (
     integerAdd(integerAdd(ai, bi), carry) + (decimalSum ? `.${decimalSum}` : "")
   );
 }
+
+/* test code */
+const assert = require("node:assert/strict");
+
+assert.equal(integerAdd("1", "9999"), integerAdd("10", "9990"));
 
 assert.equal(add("1.001", "1.999"), "3");
 assert.equal(add("0.1", "1.999"), "2.099");
