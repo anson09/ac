632adfcc268a7c798cc277dfd3da5e491347255c
anson <anson@ricequant.com>
2023-01-04 15:08:16 +0800

    feat: n_queens

--- 4_leetcode/51_n_queens.js
+++ 4_leetcode/51_n_queens.js
@@ -1,3 +1,34 @@
 // https://leetcode.cn/problems/n-queens/
 
-var solveNQueens = function (n) {};
+var solveNQueens = function (n) {
+  const rsp = [];
+  const board = [...Array(n)].map(() => Array(n).fill("."));
+  dfs(0);
+  return rsp;
+
+  function dfs(row) {
+    if (row === n) {
+      rsp.push(board.map((row) => row.join("")));
+      return;
+    }
+    for (let col = 0; col < n; col++) {
+      if (!isValid(row, col)) continue;
+      board[row][col] = "Q";
+      dfs(row + 1);
+      board[row][col] = ".";
+    }
+  }
+
+  function isValid(row, col) {
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === "Q") return false;
+    }
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] === "Q") return false;
+    }
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (board[i][j] === "Q") return false;
+    }
+    return true;
+  }
+};
