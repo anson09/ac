706c148ce25fd95d00c36be3d69cedaa5d9bb7b9
anson <anson@ricequant.com>
2023-02-21 11:17:21 +0800

    feat: Fisher–Yates shuffle

---
+++ 4_leetcode/384_shuffle_an_array.js
@@ -0,0 +1,20 @@
+// https://leetcode.cn/problems/shuffle-an-array/
+
+var Solution = function (nums) {
+  this.nums = nums;
+};
+
+Solution.prototype.reset = function () {
+  return this.nums;
+};
+
+// time: 152ms | beat: 44%
+// Fisher–Yates shuffle 洗牌算法
+Solution.prototype.shuffle = function () {
+  const nums = this.nums.slice();
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [nums[i], nums[rand]] = [nums[rand], nums[i]];
+  }
+  return nums;
+};
