b771de2c24c4a015a384da09f3587c1f6224fcf7
anson <anson@ricequant.com>
2023-03-24 10:26:29 +0800

    feat: valid parentheses

---
+++ 4_leetcode/20_valid_parentheses.js
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/valid-parentheses/
+
+// time: O(n) | 52ms | beat 99%
+function isValid(s) {
+  const pair = { "(": ")", "{": "}", "[": "]" };
+  const stack = [];
+  for (let i of s) {
+    if (Object.hasOwn(pair, i)) stack.push(i);
+    else if (i !== pair[stack.pop()]) return false;
+  }
+  return !stack.length;
+}
