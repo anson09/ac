f0cdb321030bfac3b2ac0ddff963ea6b682eb4c8
anson <anson@ricequant.com>
2023-03-29 14:13:16 +0800

    feat: get value in object

---
+++ 1_js_practice/get_value.js
@@ -0,0 +1,30 @@
+// writing a get function to pass test code below
+
+// version 1
+function get(obj, params) {
+  return eval("obj." + params);
+}
+
+// version 2
+function get(obj, params) {
+  let rsp = obj;
+  params.match(/\w/g).forEach((i) => {
+    if (/\d/.test(i)) rsp = rsp[Number(i)];
+    else rsp = rsp[i];
+  });
+  return rsp;
+}
+
+/* test code */
+const assert = require("node:assert/strict");
+
+var obj = {
+  a: {
+    b: [1, 2, 3, 4, 5],
+  },
+  c: "great",
+};
+
+assert.deepEqual(get(obj, "a.b"), [1, 2, 3, 4, 5]);
+assert.equal(get(obj, "a.b[0]"), 1);
+assert.equal(get(obj, "c"), "great");
