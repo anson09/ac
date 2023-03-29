03cdcfb867d6fd1280ee9430ca7d145c6a232d53
anson <anson@ricequant.com>
2023-03-29 17:52:47 +0800

    feat: pubsub achieve

---
+++ 1_js_practice/pubsub.js
@@ -0,0 +1,61 @@
+class PubSub {
+  #evts = {};
+
+  on(e, cb) {
+    if (!e || !cb) return;
+    this.#evts[e] ? this.#evts[e].push(cb) : (this.#evts[e] = [cb]);
+  }
+
+  once(e, cb) {
+    if (!e || !cb) return;
+    const wrapper = (...params) => {
+      cb(...params);
+      this.off(e, wrapper);
+    };
+    this.on(e, wrapper);
+  }
+
+  off(e, cb) {
+    if (!this.#has(e) || !cb) return;
+    if (cb === "*") {
+      delete this.#evts[e];
+      return;
+    }
+    this.#evts[e] = this.#evts[e].filter((i) => i !== cb);
+    !this.#evts[e].length && delete this.#evts[e];
+  }
+
+  emit(e, ...params) {
+    if (!this.#has(e)) return;
+    this.#evts[e].forEach((i) => i(...params));
+  }
+
+  #has(e) {
+    return this.#evts[e];
+  }
+
+  get debug() {
+    return this.#evts;
+  }
+}
+
+/* test code */
+const assert = require("node:assert/strict");
+
+const pubsub = new PubSub();
+
+pubsub.on("event", () => {
+  assert.ok(true);
+});
+
+pubsub.once("event", () => {
+  assert.ok(true);
+});
+
+assert.equal(pubsub.debug.event.length, 2);
+
+pubsub.emit("event");
+assert.equal(pubsub.debug.event.length, 1);
+
+pubsub.off("event", "*");
+assert.equal(pubsub.debug.event, undefined);
