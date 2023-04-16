f363fc827cd82c0b87fa694ddd93f70a5f387984
anson <anson@ricequant.com>
2023-04-17 00:03:51 +0800

    refactor: combinations

--- 4_leetcode/77_combinations.js
+++ 4_leetcode/77_combinations.js
@@ -2,6 +2,25 @@
 
 // https://leetcode.cn/problems/combinations/
 
+// time: 268ms
+var combine = function (n, k) {
+  const result = [];
+  const nums = [...Array(n)].map((_, idx) => idx + 1);
+  dfs([], nums);
+  return result;
+
+  function dfs(select, options) {
+    if (select.length === k) {
+      result.push(select);
+      return;
+    }
+
+    for (let i in options) {
+      dfs([...select, options[i]], options.slice(Number(i) + 1));
+    }
+  }
+};
+
 // 排列每层都重头找，组合下层从上一层的下一个开始找
 // time: 84ms | beat 91%
 var combine = function (n, k) {
