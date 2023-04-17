96936577bda9359c68105ac0b8b6bbea7e7e8450
anson <anson@ricequant.com>
2023-04-17 15:49:22 +0800

    refactor: intersection

--- 4_leetcode/349_intersection_of_two_arrays.js
+++ 4_leetcode/349_intersection_of_two_arrays.js
@@ -3,16 +3,14 @@
 // https://leetcode.cn/problems/intersection-of-two-arrays/
 
 // version 1
-// time: O(n) | 64ms | beat 60%
+// time: O(n) | 52ms | beat 99%
 var intersection = function (nums1, nums2) {
   const res = [];
-  const [long, short] =
-    nums1.length > nums2.length
-      ? [new Set(nums1), new Set(nums2)]
-      : [new Set(nums2), new Set(nums1)];
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
 
-  for (let i of short) {
-    long.has(i) && res.push(i);
+  for (let i of set2) {
+    set1.has(i) && res.push(i);
   }
 
   return res;
